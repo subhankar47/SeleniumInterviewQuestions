1e69c3b021bcab85430dda9cae297cfa2db5e346
SUBHANKAR <subhankarjena14@gmail.com>
2022-08-18 20:02:11 +0530

    1st my commit

--- src/test/java/MyPackage/Date_Picker.java
+++ src/test/java/MyPackage/Date_Picker.java
@@ -2,14 +2,13 @@ package MyPackage;
 
 import java.time.Duration;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class DatePicker {
+public class Date_Picker {
 
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
